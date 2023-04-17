086a6a27c92ab46a337d501db7e094710d5f67de
ibrahim abdulhemit <93004625+abdulhemit@users.noreply.github.com>
2023-04-17 18:43:19 +0300

    chatAppWithNotification

---
+++ app/src/main/java/com/example/chatappwithnotification/constants.kt
@@ -0,0 +1,11 @@
+package com.example.chatappwithnotification
+
+class constants {
+
+    companion object {
+
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAJoc4d0I:APA91bHIRNkCWeOg_NpFcRojPiK1q68Y2Sg5fX9gfSHKYIl-IoYKljYIGi21ZlXRFKv4-hlLhJ2jbWeAxdFAUWi7gg6grvIaHQU-hi5q7Bsiam3M3Xy0tXxfHZLSsrKlVXdmDSbFZVjd"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
